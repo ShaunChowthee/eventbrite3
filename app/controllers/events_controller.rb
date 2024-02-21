f8ab0c852ffa9de055abaf7436adf56814bf06bb
ShaunChowthee <sn.chowthee@gmail.com>
2024-02-21 03:56:41 +0100

    adding some views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,38 @@
 class EventsController < ApplicationController
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+  end
+
   def index
     @events = Event.all
+    @user = current_user
   end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location, :admin_id)
+  end
+  
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
