05709aaa15408afc950d44c622137bb012b40ee1
ShaunChowthee <sn.chowthee@gmail.com>
2024-02-22 06:03:26 +0100

    added navbar and sign in/up/out conditions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,14 @@
 class UserMailer < ApplicationMailer
   default from: ENV['MAILJET_DEFAULT_FROM']
+  def welcome_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
 
-  def welcome_email(new_user)
-    @user = new_user
-    @url = "https://eventbrite_like.com/login"
-    mail(to: @user.email, subject: "Welcome #{@user.first_name}")
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,15 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['MAILJET_LOGIN'],
+    :password => ENV['MAILJET_PWD'],
+    :domain => 'monsite.fr',
+    :address => 'in-v3.mailjet.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  
 
 end
